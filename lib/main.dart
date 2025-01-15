e0ecaf5496552753d719660b32401a0d96c37767
Franco <fs7995554@gmail.com>
2025-01-15 17:49:17 -0300

    Agregar Imagenes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,48 @@
 import 'package:flutter/material.dart';
-import 'src/holamundo.dart';
-void main() {
+
+void main(List<String> args) {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Imagenes",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "mostrar imagenes",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 250.0,
+                width: 500.0,
+                child: Image.asset(
+                  "assets/image.png",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              SizedBox(
+                height: 250.0,
+                width: 500.0,
+                child: Image.network(
+                  "https://i.ibb.co/hDbrkLM/logo-personal.jpg",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
